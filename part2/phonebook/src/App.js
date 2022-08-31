be1427aee84974e617e15994981ef9a9d27e44f9
plokm1234 <58145843+plokm1234@users.noreply.github.com>
2022-08-31 17:12:37 +0800

    exercises 2.11

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,17 +1,20 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import PersonForm from "./PersonForm.js"
 import Filter from "./Filter.js"
 import Persons from "./Persons.js"
+import axios from 'axios'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
 
   const [search, setSearch] = useState('')
+
+  useEffect(() => {
+      const promise = axios.get('http://localhost:3001/persons')
+      promise.then(response => {
+        setPersons(response.data)
+      })      
+    },[])
 
   const handleSubmit = (e) => {
     e.preventDefault()
